7cd24cf99fc7663333bfabb1f31c162114bfc9fc
gabriel8programmer <gabriel8github@gmail.com>
2025-05-15 13:17:46 -0300

    feat: finish stacks page

---
+++ assets/js/stacks.js
@@ -0,0 +1,43 @@
+
+const body = document.querySelector("body")
+const controlStacks = document.querySelector("#control-stacks")
+const btns = [...controlStacks.querySelectorAll("button")]
+const stackGroups = document.querySelector("#stack-groups")
+const groups = stackGroups.querySelectorAll("ul")
+
+const disableAll = () => {
+    btns.forEach(btn => {
+        btn.classList.add("text-white/50")
+        btn.classList.remove("border-b-5")
+    })
+
+    groups.forEach(group => {
+        if (!group.classList.contains("hidden")) {
+            group.classList.add("hidden")
+        }
+    })
+}
+
+const updateBackground = (i) => {
+    body.setAttribute("class", "h-screen bg-cover bg-center flex flex-col")
+
+    body.classList.add(`bg-[linear-gradient(to_bottom,_black,_rgba(0,0,0,0.5),_black),url('../img/bg-${i + 2}.jpg')]`)
+}
+
+const enableStackControl = (e, i) => {
+    const btn = e.target;
+
+    // disable all controls
+    disableAll()
+
+    // add hilight only active component 
+    btn.classList.remove("text-white/50")
+    btn.classList.add("border-b-5")
+    // add stack group enabled
+    groups[i].classList.remove("hidden")
+
+    // change background
+    updateBackground(i)
+}
+
+btns.forEach((btn, i) => btn.addEventListener("click", (e) => enableStackControl(e, i)))
